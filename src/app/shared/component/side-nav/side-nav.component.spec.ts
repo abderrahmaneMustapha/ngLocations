b3ec756f6e67324223f543e86fa2cb73df5c5e39
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2022-11-07 14:37:17 +0100

    test: write tests for shared components

--- src/app/shared/component/side-nav/side-nav.component.spec.ts
+++ src/app/shared/component/side-nav/side-nav.component.spec.ts
@@ -1,29 +1,47 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DxDrawerComponent, DxDrawerModule, DxListModule } from 'devextreme-angular';
 import { ConfigService } from 'src/app/config/config.service';
 import { DataService } from 'src/app/core/service/data.service';
 import { NavService } from 'src/app/core/service/nav.service';
 
 import { SideNavComponent } from './side-nav.component';
+import { SideNavService } from './side-nav.service';
 
 describe('SideNavComponent', () => {
   let component: SideNavComponent;
+  let service: SideNavService
   let fixture: ComponentFixture<SideNavComponent>;
+  let navLinks;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientModule],
-      providers: [ConfigService, DataService, NavService],
-      declarations: [ SideNavComponent ]
+      imports: [HttpClientModule, DxDrawerModule, DxListModule],
+      providers: [ConfigService, DataService, NavService, SideNavService],
+      declarations: [SideNavComponent]
     })
     .compileComponents();
 
+    service = new SideNavService()
     fixture = TestBed.createComponent(SideNavComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('count navigation links', () => {
+    fixture.detectChanges()
+    navLinks = fixture.nativeElement.querySelectorAll('.dx-theme-text-color')
+    expect(navLinks.length).toBe(service.getNavigationList().length)
+  })
+
+  it('side nav bar should hide', () => {
+    component.isDrawerOpen = false
+    fixture.detectChanges()
+    let drawer = fixture.debugElement.query(By.css('dx-drawer'));
+    expect(drawer.attributes['ng-reflect-opened']).toBe('false')
+  })
 });
--- src/app/shared/component/top-nav/top-nav.component.spec.ts
+++ src/app/shared/component/top-nav/top-nav.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DxButtonModule, DxToolbarModule } from 'devextreme-angular';
 import { ConfigService } from 'src/app/config/config.service';
 import { DataService } from 'src/app/core/service/data.service';
 import { NavService } from 'src/app/core/service/nav.service';
@@ -7,23 +8,33 @@ import { NavService } from 'src/app/core/service/nav.service';
 import { TopNavComponent } from './top-nav.component';
 
 describe('TopNavComponent', () => {
-  let component: TopNavComponent;
-  let fixture: ComponentFixture<TopNavComponent>;
+  let component: TopNavComponent
+  let service: NavService
+  let fixture: ComponentFixture<TopNavComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [HttpClientModule, DxToolbarModule, DxButtonModule],
       providers: [ConfigService, DataService, NavService],
       declarations: [ TopNavComponent ]
     })
-    .compileComponents();
+    .compileComponents()
 
-    fixture = TestBed.createComponent(TopNavComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture = TestBed.createComponent(TopNavComponent)
+    service = new NavService()
+    component = fixture.componentInstance
+    fixture.detectChanges()
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    expect(component).toBeTruthy()
+  })
+
+  it('test close and open on button click', () => {
+    let element: HTMLElement = fixture.nativeElement;
+    let html5Button = element.querySelector("dx-button-normal") as HTMLElement;
+    html5Button?.click();
+    fixture.detectChanges();
+    expect(service.isDrawerOpen).toBe(true)
+  })
 });
